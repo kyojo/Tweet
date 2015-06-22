4fb9accbc8f5a23afdf44e1127a8f739dd7a3ee3
kei <kyojo5764@gmail.com>
2015-06-22 11:34:40 +0900

    0622

--- claasify2.rb
+++ claasify2.rb
@@ -6,17 +6,36 @@ require 'stemmer'
 
 begin
   op = nil
-  File.open("bayes_data", "r") do |f|
+  co = nil
+  ex = nil
+  ag = nil
+  ne = nil
+  File.open("bayes_op2", "r") do |f|
     op = Marshal.load(f)
+  end
+  File.open("bayes_co2", "r") do |f|
+    co = Marshal.load(f)
+  end
+  File.open("bayes_ex2", "r") do |f|
+    ex = Marshal.load(f)
+  end
+  File.open("bayes_ag2", "r") do |f|
+    ag = Marshal.load(f)
+  end
+  File.open("bayes_ne2", "r") do |f|
+    ne = Marshal.load(f)
   end
 rescue
   op = Classifier::Bayes.new("0","1")
+  co = Classifier::Bayes.new("0","1")
+  ex = Classifier::Bayes.new("0","1")
+  ag = Classifier::Bayes.new("0","1")
+  ne = Classifier::Bayes.new("0","1")
 end
 
 me = MeCab::Tagger.new
 
-correct = 0
-fault = 0
+optr = opfa = cotr = cofa = extr = exfa = agtr = agfa = netr = nefa =  0
 
 Dir::glob("/Users/kei/tweet/sampling/**/*.csv").each do |f|
   pass = f.split("/")
@@ -49,22 +68,56 @@ Dir::glob("/Users/kei/tweet/sampling/**/*.csv").each do |f|
   end
 
   doc = words.join(" ")
-  result = op.classify(doc).to_i
+  opre = op.classify(doc).to_i
+  core = co.classify(doc).to_i
+  exre = ex.classify(doc).to_i
+  agre = ag.classify(doc).to_i
+  nere = ne.classify(doc).to_i
   CSV.foreach("per.csv") do |per|
     if id == per[0].to_i
-      if(per[1].to_i < 30 && result == 0)
-        correct += 1
-      elsif(per[1].to_i >= 30 && result == 1)
-        correct += 1
+      if(per[1].to_i < 30 && opre == 0)
+        optr += 1
+      elsif(per[1].to_i >= 30 && opre == 1)
+        optr += 1
       else
-        fault += 1
+        opfa += 1
+      end
+      if(per[2].to_i < 30 && core == 0)
+        cotr += 1
+      elsif(per[1].to_i >= 30 && core == 1)
+        cotr += 1
+      else
+        cofa += 1
+      end
+      if(per[3].to_i < 30 && exre == 0)
+        extr += 1
+      elsif(per[1].to_i >= 30 && exre == 1)
+        extr += 1
+      else
+        exfa += 1
+      end
+      if(per[4].to_i < 30 && agre == 0)
+        agtr += 1
+      elsif(per[1].to_i >= 30 && agre == 1)
+        agtr += 1
+      else
+        agfa += 1
+      end
+      if(per[5].to_i < 30 && nere == 0)
+        netr += 1
+      elsif(per[1].to_i >= 30 && nere == 1)
+        netr += 1
+      else
+        nefa += 1
       end
       break
     end
   end
 end
 
-puts correct
-puts fault
-pro = (correct / (correct + fault)) * 100.to_f
-puts "#{pro}%"
+oppr = (optr / (optr + opfa)) * 100.to_f
+copr = (cotr / (cotr + cofa)) * 100.to_f
+expr = (extr / (extr + exfa)) * 100.to_f
+agpr = (agtr / (agtr + agfa)) * 100.to_f
+nepr = (netr / (netr + nefa)) * 100.to_f
+puts "#{oppr}%, #{copr}%, #{expr}%, #{agpr}%, #{nepr}%"

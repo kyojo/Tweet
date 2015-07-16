38db593b26dd658f062a5ea7d28567f5aa83d3f3
kei <kyojo5764@gmail.com>
2015-07-16 14:36:21 +0900

    0716

--- infogain.rb
+++ infogain.rb
@@ -64,7 +64,7 @@ Dir::glob("/Users/kei/tweet/sampling/**/*.csv").each do |f|
     end
   end
 
-  #Infogain
+  #word count
   CSV.foreach("per.csv") do |per|
     if id == per[0].to_i
       for wd in words
@@ -86,6 +86,7 @@ end
 infogain = Array.new(5){{}}
 allwd.uniq!
 allwd.delete("*")
+eps = 1e-10
 for wd in allwd
   for n in 0..4
     p_p = np[n] / (np[n] + nn[n]).to_f
@@ -97,31 +98,32 @@ for wd in allwd
     p_p0 = (np[n] - npw[n][wd]) / (np[n] - npw[n][wd] + nn[n] - nnw[n][wd]).to_f
     p_n0 = 1.0 - p_p0
     h_c = -1.0 * p_p * log(p_p) - p_n * log(p_n)
-    if p_n1.to_i == 0
+    if (p_n1-0.0).abs <= eps
       h_c1 = -1.0 * p_p1 * log(p_p1)
-    elsif p_p1.to_i == 0
+    elsif (p_p1-0.0).abs <= eps
       h_c1 = -1.0 * p_n1 * log(p_n1)
     else
       h_c1 = -1.0 * p_p1 * log(p_p1) - p_n1 * log(p_n1)
     end
-    if p_n0.to_i == 0
+    if (p_n0-0.0).abs <= eps
       h_c0 = -1.0 * p_p0 * log(p_p0)
-    elsif p_p0.to_i == 0
+    elsif (p_p0-0.0).abs <= eps
       h_c0 = -1.0 * p_n0 * log(p_n0)
     else
       h_c0 = -1.0 * p_p0 * log(p_p0) - p_n0 * log(p_n0)
     end
     gain = h_c - (p1 * h_c1 + p0 * h_c0)
-    #puts "#{wd},#{n} : #{h_c}, #{h_c1}, #{h_c0}, #{gain}"
+    #puts "#{wd},#{n} : #{h_c}, #{p1}, #{h_c1}, #{p0}, #{h_c0}, #{gain}"
+    #puts "#{wd},#{n} : #{h_c}, #{p_p}, #{h_c0}, #{p_p0}"
     infogain[n][wd] = gain
   end
 end
 
 result = Array(2)
 print "Openness,infogain,Conscientiousness,infogain,Extraversion,infogain,Agreeableness,infogain,Neuroticism,infogain\n"
-for i in 0..50
+for i in 0..500
   for n in 0..4
-    result = infogain[n].sort{|(k1, v1), (k2, v2)| (v2*100000).to_i <=> (v1*100000).to_i}[i]
+    result = infogain[n].sort{|(k1, v1), (k2, v2)| v2 <=> v1}[i]
     print "#{result[0]},#{result[1]}"
     if n != 4
       print ","
--- temp.rb
+++ temp.rb
@@ -1,5 +1,5 @@
 include Math
-puts (0.0).to_i
-puts (-0.00001).to_i
-puts (0.000001).to_i
-puts log(2.8)
+
+a = 0.9/9 - 0.1
+
+puts (a-0.0).abs <= 1e-10
